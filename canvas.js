e987cbfcc07ae7de697109ce72d67826018ad689
johnmurph27 <jomu8186@colorado.edu>
2018-12-13 21:05:35 -0700

    working bounce and color change with monogram.

--- canvas.js
+++ canvas.js
@@ -1,4 +1,8 @@
-
+const colors = ["#252839", "#e05038", "#677077", "#b5b5b7", "#f2b632", "#300032","#c43235", "#f6f1ed", "#6534ff", "#5e0231"];
+var x = 20;
+var y = 200;
+var dx = .5;
+var dy = .5;
 
 var container = document.getElementById("container");
 var width = document.body.clientWidth;
@@ -7,9 +11,12 @@ container.setAttribute("style","width:"+width+"px; height:"+height+"px; border: 
 
 // Resize container on window resize
 var onresize = function() {
-   
-    var width = document.body.clientWidth;
-    var height = document.body.clientHeight;
+    width = document.body.clientWidth;
+    height = document.body.clientHeight;
+    if(x > width || height < y){
+        x = 0;
+        y = 50;
+    }
     container.setAttribute("style","width:"+width+"px; height:"+height+"px; border: 1px solid black");
 
 }
@@ -17,23 +24,43 @@ var onresize = function() {
  window.addEventListener("resize", onresize);
 
 
- var x = 20;
- var y = 200;
- var dx = 1;
- var dy = 1;
+
+ 
  
 
 function move() {
     var box = document.getElementById("box");
-    var id = setInterval(frame, 10);
+    var text = document.getElementsByClassName("colored");
+    console.log(text);
+    
+    var newColor;
+    var id = setInterval(frame, 1);
     function frame() {
-        if(x + 50 > width || x < 0){
+        if(x + 300 > width || x < 0){
+            newColor = colors[Math.floor(Math.random() * 10)];
+            for(i=0; i < text.length; i++){
+                if(text[i].nodeName == "DIV"){
+                    text[i].style.backgroundColor = newColor;
+                }else {
+                    text[i].style.color = newColor;
+                }
+            }
             dx = -dx;
         } else {
             
             box.style.left = x + "px";
         }
-        if ( y + 50 > height || y < 0){
+        if ( y + 186 > height || y < 0){
+            newColor = colors[Math.floor(Math.random() * 10)];
+            for(i=0; i < text.length; i++){
+                if(text[i].nodeName == "DIV"){
+                    text[i].style.backgroundColor = newColor;
+                }else {
+                    text[i].style.color = newColor;
+                }
+            }
+            
+            
             dy = -dy;
         }else {
             box.style.top = y + "px";
@@ -43,5 +70,6 @@ function move() {
     }
 }
 move();
+
 
 
